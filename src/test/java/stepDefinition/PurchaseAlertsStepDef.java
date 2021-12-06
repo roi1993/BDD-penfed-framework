241b5bdab7b8b3d32599a9126ff78f142fb47aab
Konul Salimova <salimovakonul@gmail.com>
2021-12-06 16:06:15 -0500

    PurchaseAlerts.feature, PAge and StepDef first commit

---
+++ src/test/java/stepDefinition/PurchaseAlertsStepDef.java
@@ -0,0 +1,18 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+import pages.PurchaseAlertsPage;
+import utilities.Driver;
+
+public class PurchaseAlertsStepDef {
+
+    PurchaseAlertsPage purchaseAlertsPage = new PurchaseAlertsPage();
+
+
+    @Given("I click on  the link below Purchase Alerts")
+    public void i_click_on_the_link_below_purchase_alerts() {
+        purchaseAlertsPage.purchaseAlerts.click();
+        Assert.assertEquals(Driver.getDriver().getTitle(),"VISA");
+    }
+}
