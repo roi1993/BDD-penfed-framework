8b210797cd042fa4d3c92538396458402eda2084
Kateryna Roi <kateryna782@gmail.com>
2021-12-06 15:57:32 -0500

    Kateryna- commit for Zilola2

--- src/test/java/stepDefinition/MoneyMarketSavingsCalculatorStepDef.java
+++ src/test/java/stepDefinition/MoneyMarketSavingsCalculatorStepDef.java
@@ -44,4 +44,90 @@ public class MoneyMarketSavingsCalculatorStepDef {
 
 
     }
+
+    @Then("I am able to enter alphabetical characters for initial amount and submit to see my earnings")
+    public void i_am_able_to_enter_alphabetical_characters_for_initial_amount_and_submit_to_see_my_earnings() {
+        // Write code here that turns the phrase above into concrete actions
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.click();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.sendKeys("1234");
+        //throw new io.cucumber.java.PendingException();
+        moneyMarketSavingsCalculatorPage.initialAmount.click();
+        moneyMarketSavingsCalculatorPage.initialAmount.clear();
+        moneyMarketSavingsCalculatorPage.initialAmount.sendKeys("shsbfhdhsdf");
+        moneyMarketSavingsCalculatorPage.numberOfYears.click();
+        moneyMarketSavingsCalculatorPage.numberOfYears.clear();
+        moneyMarketSavingsCalculatorPage.numberOfYears.sendKeys("3");
+        moneyMarketSavingsCalculatorPage.calculateButton.click();
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("I am able to enter alphabetical characters for the number of years and submit to see my earnings")
+    public void i_am_able_to_enter_alphabetical_characters_for_the_number_of_years_and_sumbit_to_see_my_earnings() {
+        // Write code here that turns the phrase above into concrete actions
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.click();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.sendKeys("12345");
+        moneyMarketSavingsCalculatorPage.initialAmount.click();
+        moneyMarketSavingsCalculatorPage.initialAmount.clear();
+        moneyMarketSavingsCalculatorPage.initialAmount.sendKeys("1234");
+        moneyMarketSavingsCalculatorPage.numberOfYears.click();
+        moneyMarketSavingsCalculatorPage.numberOfYears.clear();
+        moneyMarketSavingsCalculatorPage.numberOfYears.sendKeys("dsfbdshkbf");
+        moneyMarketSavingsCalculatorPage.calculateButton.click();
+        //  throw new io.cucumber.java.PendingException();
+    }
+    @Then("I am able to enter numerical characters for monthly additions")
+    public void i_am_able_to_enter_numerical_characters_for_monthly_additions() {
+        // Write code here that turns the phrase above into concrete actions
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.click();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.sendKeys("1234");
+        //throw new io.cucumber.java.PendingException();
+        moneyMarketSavingsCalculatorPage.initialAmount.click();
+        moneyMarketSavingsCalculatorPage.initialAmount.clear();
+        moneyMarketSavingsCalculatorPage.initialAmount.sendKeys("sfjnsdkf");
+        moneyMarketSavingsCalculatorPage.numberOfYears.click();
+        moneyMarketSavingsCalculatorPage.numberOfYears.clear();
+        moneyMarketSavingsCalculatorPage.numberOfYears.sendKeys("dskfdsb");
+        moneyMarketSavingsCalculatorPage.calculateButton.click();
+        // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I am able to enter numerical characters for the initial amount")
+    public void i_am_able_to_enter_numerical_characters_for_the_initial_amount() {
+        // Write code here that turns the phrase above into concrete actions
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.click();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.sendKeys("asudkhr32784");
+        //throw new io.cucumber.java.PendingException();
+        moneyMarketSavingsCalculatorPage.initialAmount.click();
+        moneyMarketSavingsCalculatorPage.initialAmount.clear();
+        moneyMarketSavingsCalculatorPage.initialAmount.sendKeys("1234");
+        moneyMarketSavingsCalculatorPage.numberOfYears.click();
+        moneyMarketSavingsCalculatorPage.numberOfYears.clear();
+        moneyMarketSavingsCalculatorPage.numberOfYears.sendKeys("dskfdsb");
+        moneyMarketSavingsCalculatorPage.calculateButton.click();
+        // throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I am able to enter numerical characters for the number of years")
+    public void i_am_able_to_enter_numerical_characters_for_the_number_of_years() {
+
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.click();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.clear();
+        moneyMarketSavingsCalculatorPage.monthlyAdditions.sendKeys("knsdfjdk24324");
+
+        moneyMarketSavingsCalculatorPage.initialAmount.click();
+        moneyMarketSavingsCalculatorPage.initialAmount.clear();
+        moneyMarketSavingsCalculatorPage.initialAmount.sendKeys("sdfjdsfdf3242");
+        moneyMarketSavingsCalculatorPage.numberOfYears.click();
+        moneyMarketSavingsCalculatorPage.numberOfYears.clear();
+        moneyMarketSavingsCalculatorPage.numberOfYears.sendKeys("12345");
+        moneyMarketSavingsCalculatorPage.calculateButton.click();
+
+    }
+
 }
--- src/test/resources/MoneyMarketSavingsCalculator.feature
+++ src/test/resources/MoneyMarketSavingsCalculator.feature
@@ -2,9 +2,27 @@
 Feature: As a user, I should be able to arrive on the Money Market Savings Calculator page,
   so that i can use a calculator to see my savings.
 
+  Background:
+    Given I`m on home page
+    Then I choose Learn
+    Then I click on Calculators
+    Then I click on Money Market Savings Calculator
+
     Scenario: Go to Money Market Savings Calculator page
-      Given I`m on home page
-      Then I choose Learn
-      Then I click on Calculators
-      Then I click on Money Market Savings Calculator
       Then I am able to fill out the calculator and submit to see my earnings
+
+
+  Scenario: Enter alphabetical characters for the initial amount
+    Then I am able to enter alphabetical characters for initial amount and submit to see my earnings
+
+  Scenario: Enter alphabetical characters for the number of years
+    Then I am able to enter alphabetical characters for the number of years and submit to see my earnings
+
+  Scenario: Enter numerical characters for the monthly additions
+    Then I am able to enter numerical characters for monthly additions
+
+  Scenario: Enter numerical characters for the initial amount
+    Then I am able to enter numerical characters for the initial amount
+
+  Scenario: Enter numerical characters for the number of years
+    Then I am able to enter numerical characters for the number of years
